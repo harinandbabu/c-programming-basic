ee8f0b45a2ff6f54a1b83cbf112406113151a318
Harinand Babu <harinandbabu@gmail.com>
2026-07-16 20:21:07 +0530

    Difference of two numbers

---
+++ substract.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter a number");
+    scanf("%d",&a);
+    printf("Enter another number");
+    scanf("%d",&b);
+    c=a-b;
+    printf("Substraction=%d",c);
+}
