de9b0d6a1efccbdbafcd0f7ffd90a513b4733fa5
Harinand Babu <harinandbabu@gmail.com>
2026-07-17 20:02:03 +0530

    finding letter is vowel or vowel_or_consonants

---
+++ vowel_or_consonants.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    char v;
+    printf("Enter a letter:");
+    scanf("%c",&v);
+    if(v=='a')
+    {
+        printf("Letter is vowel");
+    }
+    else if(v=='e')
+    {
+        printf("Letter is vowel");
+    }
+    else if(v=='i')
+    {
+        printf("Letter is vowel");
+    }
+    else if(v=='o')
+    {
+        printf("Letter is vowel");
+    }
+    else if(v=='u')
+    {
+        printf("Letter is vowel");
+    }
+    else
+    {
+        printf("Letter is consonant");
+    }
+}
