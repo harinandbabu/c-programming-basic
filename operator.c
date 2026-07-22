1f2657b0247292d31e5c457e32d6f89c16a72913
Harinand Babu <harinandbabu@gmail.com>
2026-07-22 20:06:54 +0530

    maths operators

--- operator.c
+++ operator.c
@@ -1,1 +1,36 @@
 #include<stdio.h>
+int main()
+{
+    char op;
+    float a,b;
+    printf("Enter operator:\n");
+    scanf("%c",&op);
+    printf("Enter two numbers:\n");
+    scanf("%f %f",&a,&b);
+    switch(op)
+    {
+        case '+':
+        printf("Sum=%.2f",a+b);
+        break;
+        case '-':
+        printf("Difference=%.2f",a-b);
+        break;
+        case '*':
+        printf("Multiplication=%.2f",a*b);
+        break;
+        case '/':
+            if (b==0)
+            {
+                printf("Division not possible");
+            }
+            else
+            {
+                printf("Division=%.2f",a/b);
+            }
+        break;
+        default:
+        printf("Invalid operator");
+        break;
+    }
+return 0;
+}
